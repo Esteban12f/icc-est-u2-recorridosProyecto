c81042e02efcbcc6525fc2c68fe6b67f68c01da0
Esteban12f <joehv33@gmail.com>
2025-02-19 00:11:53 -0500

    Terminado

--- src/App.java
+++ src/App.java
@@ -1,22 +1,13 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
 
 import controllers.ControllerView;
-import controllers.MazeSolverBFS;
-import controllers.MazeSolverDP;
-import controllers.MazeSolverRecursivo;
-import controllers.interfaces.MazeSolver;
-import models.Cell;
-import models.Maze;
 import views.ViewMain;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        ViewMain viewMain = new ViewMain();
-        ControllerView controllerView = new ControllerView(viewMain);
-        viewMain.setVisible(true);
+
+        ViewMain vistaPrincipal = new ViewMain();
+        ControllerView controlVista = new ControllerView(vistaPrincipal);
+        vistaPrincipal.setVisible(true);
         
     }
     
